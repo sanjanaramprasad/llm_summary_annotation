b54ea52ebd64fe48c30fd994a4c43ba1127dcc9a
EC2 Default User <ec2-user@ip-172-31-24-231.ec2.internal>
2023-05-28 00:12:13 +0000

    updates

--- app.py
+++ app.py
@@ -17,17 +17,19 @@ import spacy
 nlp = spacy.load('en_core_web_sm')
 # import flask_monitoringdashboard as dashboard
 
-app = Flask(__name__)
+#app = Flask(__name__)
+application = Flask(__name__)
 # dashboard.bind(app)
-app.secret_key = 'your_secret_key_here'
+
+application.secret_key = 'your_secret_key_here'
 
 
 
 
-directory = '/home/ec2-user/llm_summary_annotation/data'
-database_name = 'summaries_news_sample.db'
+directory = '/home/ec2-user/human_annotations_factuality/XSUM_CNN/set1_annotations'
+database_name = 'news_summaries_set1.db'
 db_path = '/%s/%s'%(directory, database_name)
-db_engine = dbEngine=sqlalchemy.create_engine('sqlite:////home/ec2-user/llm_summary_annotation/data/summaries_news_sample.db')
+db_engine = dbEngine=sqlalchemy.create_engine('sqlite:///' + db_path)
 
 metadata = MetaData(bind=db_engine)
 metadata.reflect()
@@ -36,6 +38,7 @@ generated_summaries = metadata.tables['generated_summaries']
 label = metadata.tables['label']
 # index1 = Index('idx_generated_summaries', generated_summaries.c.summary_uuid)
 # index2 = Index('idx_label', label.c.summary_uuid)
+# index3 = Index('idx_label', label.c.summary_uuid)
 # index1.create(bind=db_engine)
 # index2.create(bind=db_engine)
 
@@ -46,7 +49,7 @@ n_docs = 5
 
 
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(application)
 
 
 
@@ -80,31 +83,8 @@ def get_users():
     "sanjana": User(1, "sanjana", "zR46"),
     "byron": User(2, "byron", "fJ89"),
     "kundan": User(3, "kundan", "8f*7"),
-    "ann_fslj": User(4, "ann_fslj", "axdu"),
-    "ann_eqgt": User(5, "ann_eqgt", "depz"),
-    "ann_ybmo": User(6, "ann_ybmo", "vntp"),
-    "ann_dfey": User(7, "ann_dfey", "ymmr"),
-    "ann_xdjw": User(8, "ann_xdjw", "ghmf"),
-    "ann_rict": User(9, "ann_rict", "yyjx"),
-    "ann_liwk": User(10, "ann_liwk", "msxd"),
-    "ann_wqap": User(11, "ann_wqap", "wwiv"),
-    "ann_qbqn": User(12, "ann_qbqn", "cwun"),
-    "ann_zxyw": User(13, "ann_zxyw", "jclb"),
-    "ann_xkym": User(14, "ann_xkym", "lctu"),
-    "ann_aluf": User(15, "ann_aluf", "sykj"),
-    "ann_japq": User(16, "ann_japq", "bcxw"),
-    "ann_tpfo": User(17, "ann_tpfo", "ydvl"),
-    "ann_ghbn": User(18, "ann_ghbn", "ebvd"),
-    "ann_ohjd": User(19, "ann_ohjd", "pwhf"),
-    "ann_rjmd": User(20, "ann_rjmd", "fkil"),
-    "ann_fjfvtn": User(21, "ann_fjfvtn", "dwgo"),
-    "ann_vzxvqm": User(22, "ann_vzxvqm", "ttsh"),
-    "ann_wumbam" : User(22, "ann_wumbam", "plangb"),
-    "ann_dambmb" : User(22, "ann_dambmb", "ksmjrl"),
-    "ann_tajfrq" : User(22, "ann_tajfrq", "gkfimr"),
-    "ann_ecgvex" : User(22, "ann_ecgvex", "jmybgd"),
-    "ann_eayfvi" : User(22, "ann_eayfvi", "hynauj"),
-    "ann_lpagod" : User(22, "ann_lpagod", "lzwllo")
+    "ann_japq": User(4, "ann_japq", "bcxw"),
+    "ann_tpfo": User(5, "ann_tpfo", "ydvl"),
     }
     return users
 
@@ -113,7 +93,7 @@ def load_user(username):
     return get_users().get(username)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@application.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return next()
@@ -132,7 +112,7 @@ def login():
     return render_template('login.html')
 
 @login_required
-@app.route('/')
+@application.route('/')
 def hello():
     return redirect(url_for('login'))
 
@@ -140,14 +120,14 @@ def hello():
 
 
 
-@app.route("/download")
+@application.route("/download")
 def download_file():
     return send_from_directory(
         directory, database_name, as_attachment=True
     )
 
 
-@app.route('/logout')
+@application.route('/logout')
 @login_required
 def logout():
     login_link = url_for('login')
@@ -167,7 +147,7 @@ def get_summary_article_for_uid(summary_uuid) -> Tuple[str]:
     return article, summary, summ_uuid, summ_id, system_id
 
 @login_required
-@app.route('/annotate/<uid>')
+@application.route('/annotate/<uid>')
 def annotate(summary_uuid):
     # uid is a unique identifier for a *generated*
     # summary. 
@@ -175,16 +155,12 @@ def annotate(summary_uuid):
     article, summary, summ_uuid, summ_id, system_id= get_summary_article_for_uid(summary_uuid)
     summ_sents = list(nlp(summary).sents)
 
-    
-    
-
     return render_template("annotate.html", username = username, article= article, summary = summary, summ_sents = summ_sents,  summ_uuid = summ_uuid, summ_id = summ_id, system_id = system_id)
 
 @login_required
 def back(current_uuid):
         with dbEngine.connect() as con:
         
-        # print(con.execute("SELECT * FROM label").fetchall())
             username = current_user.username
             # sql_query = text(f"""SELECT summary_uuid FROM label WHERE label.user_id = '{username}' ORDER BY summary_uuid;""")
             stmt = select(label.c.summary_uuid).where(label.c.user_id == username).order_by(label.c.summary_uuid)
@@ -210,10 +186,10 @@ def next():
         # q_str = text(f"""SELECT summary_uuid FROM generated_summaries WHERE NOT EXISTS (
         #             SELECT * FROM label WHERE generated_summaries.summary_uuid = label.summary_uuid AND label.user_id = '{username}' ) 
         #               ORDER BY summary_uuid, RANDOM() LIMIT 1;""")
-        # subquery = select([label]).where(and_(generated_summaries.c.summary_uuid == label.c.summary_uuid, label.c.user_id == username))
-        # stmt = select(generated_summaries.c.summary_uuid).where(~exists(subquery)).order_by(generated_summaries.c.summary_uuid).limit(1)
+        subquery = select([label]).where(and_(generated_summaries.c.summary_uuid == label.c.summary_uuid, label.c.user_id == username))
+        stmt = select(generated_summaries.c.summary_uuid).where(~exists(subquery)).order_by(generated_summaries.c.summary_uuid).limit(1)
         with dbEngine.connect() as con:
-            stmt = select(generated_summaries.c.summary_uuid).limit(1)
+            #stmt = select(generated_summaries.c.summary_uuid).limit(1)
             summary_uuid = con.execute(stmt).fetchone()
 
         if summary_uuid is None:
@@ -223,7 +199,7 @@ def next():
         return annotate(summary_uuid[0])
 
 @login_required
-@app.route('/save_annotation', methods = ['POST'])
+@application.route('/save_annotation', methods = ['POST'])
 def save_annotation():
     # print('Annotated by ' , current_user.username)
     # relevancy_score  = int(request.form['likert_relevance'])
@@ -275,4 +251,4 @@ def save_annotation():
     
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    application.run(host='0.0.0.0', port=8080)
